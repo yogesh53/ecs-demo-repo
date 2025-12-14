97055c20d05c310e89f8fe3e9881b526c627b756
Yogesh Kumar1 <yogesh.kumar1@q3infotech.net>
2025-12-14 17:21:53 +0530

    reruning the pipeline for verification

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from my ECS Fargate DevOps Project! I am Yogesh an aspiring devops engineer"
+    return "This the test run"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
